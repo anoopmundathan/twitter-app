4a4ae63801ac16b4fced7529d7356e64047f02c5
anoopmundathan <anoop.mundathan@gmail.com>
2017-03-13 06:57:42 +0000

    Setup webpack

---
+++ public/scripts/app.bundle.js
@@ -0,0 +1,12 @@
+webpackJsonp([0],{
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+var angular = __webpack_require__(0);
+
+angular.module('app', []);
+
+/***/ })
+
+},[2]);
